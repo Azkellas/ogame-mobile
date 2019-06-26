3e4e0635d84abbe83f99732950b5e29de0b39de6
Azke <e.desbois@hotmail.fr>
2019-06-26 17:44:00 +0200

    Main architecture and sidemenus

--- common/scripts/at-runstart.js
+++ common/scripts/at-runstart.js
@@ -1,9 +1,88 @@
 'use strict';
 
-var appendMenuButton = setInterval(() => {
-    if ($('#mmonetbar').length) {
-        $('#mmonetbar').css('display', 'none');
-        console.log('hide;');
-        clearInterval(appendMenuButton);
-    }
-}, 10);
+(($) => {
+    var preventZoom = setInterval(() => {
+        if ($('head').length) {
+            $('head').append(`<meta meta name="viewport" content=  
+            "width=device-width, user-scalable=no" />`);
+            clearInterval(preventZoom);
+        }
+    });
+
+    var hideFooter = setInterval(() => {
+        if ($('#siteFooter').length) {
+            $('#siteFooter').css('display', 'none');
+            clearInterval(hideFooter);
+        }
+    });
+
+    var hideTop = setInterval(() => {
+        if ($('#info').length) {
+            $('#info').css('display', 'none');
+            clearInterval(hideTop);
+        }
+    });
+
+    var hideMenu = setInterval(() => {
+        if ($('#toolbarcomponent').length) {
+            $('#toolbarcomponent').css('display', 'none');
+            clearInterval(hideMenu);
+        }
+    });
+
+    var timeSpent = 0;
+    var appendMenuButton = setInterval(() => {
+        if ($('#mmoContent').length && $('#mmonetbar').length && ($('#mmoContent').css('display') === 'block' || $('#mmonetbar').css('display') === 'block')) {
+            $('#mmonetbar').css('display', 'none');
+            $('#mmoContent').css('display', 'none');
+            timeSpent += 10;
+            console.log('hide;');
+        }
+        if (timeSpent > 1000) {
+            clearInterval(appendMenuButton);
+        }
+    }, 10);
+
+    var moveMoons = setInterval(() => {
+        if ($('.smallplanet .moonlink').length) {
+            // better alignement
+            $('.smallplanet').each(function(id, smallplanet) {
+                // background color
+                if ($(smallplanet).find('.active').length) {
+                    if ($(smallplanet).find('.moon_active').length) {
+                        $(smallplanet).css('background-color', 'rgba(142, 94, 0, 0.3)');
+                    } else {
+                        $(smallplanet).css('background-color', 'rgba(94, 142, 0, 0.3)');
+                        // outer glow
+                        $(smallplanet).find('.planetPic')
+                            .css('box-shadow',  '0 0 6px 1px #9C0, inset 0 0 3px 4px #9C0');
+                    }
+                }
+
+                // change moon hover glow
+                let baseColor = '';
+                if ($(smallplanet).find('.moon_active').length) {
+                    baseColor = $(smallplanet).find('.planet-name').css('color');
+                }
+
+                $(smallplanet).find('.moonlink').hover(
+                    function() {
+                        $(this).find('img').css('box-shadow',  '0 0 6px 1px #C90, inset 0 0 3px 4px #C90');
+                        $(smallplanet).find('.planet-name')
+                            .css('color', '#C90');
+                        $(smallplanet).find('.planet-koords')
+                            .css('color', '#C90');
+                    },
+                    function() {
+                        $(this).find('img').css('box-shadow',  '');
+                        $(smallplanet).find('.planet-name')
+                            .css('color', baseColor);
+                        $(smallplanet).find('.planet-koords')
+                            .css('color', baseColor);
+                    }
+                );
+            });
+            clearInterval(moveMoons);
+        }
+    }, 10);
+})(jQuery);
--- common/scripts/sidemenus.js
+++ common/scripts/sidemenus.js
@@ -1,45 +1,70 @@
 /* globals Slideout */
 'use strict';
 
-console.log('Im in');
-const sidemenu = `
-<nav id="leftMenu">
-    <header>
-        <h2>Menu</h2>
-    </header>
-</nav>
+(($) => {
+    var createWrapper = setInterval(() => {
+        if ($('body').length) {
+            $('body').append('<div id="oMobileWrapper"></div>');
+            clearInterval(createWrapper);
+            $('#oMobileWrapper').css('top', '0px');
+        }
+    }, 10);
 
-<nav id="rightMenu">
-    <header>
-        <h2>Menu</h2>
-    </header>
-</nav>
 
-<main id="panel">
-    <header>
-        <h2>Panel</h2>
-    </header>
-</main>
-`;
+    var moveResources = setInterval(() => {
+        if ($('#oMobileWrapper').length && $('ul#resources').length && $('#message-wrapper').length) {
+            $('#oMobileWrapper')
+                .prepend($('#message-wrapper'))
+                .prepend($('ul#resources'));
+            $('#message-wrapper').css('position', 'initial');
+            $('#message_collapsed').css('width', '50vw');
+            $('#message_collapsed > event_list > event_list').css('display', 'flex');
+            $('#attack_alert')
+                .css('right', '0px')
+                .css('position', 'absolute');
+            clearInterval(moveResources);
+        }
+    });
 
-var createMenu = setInterval(() => {
-    if ($('body').length) {
-        $('body').append(sidemenu);
+    var createMenu = setInterval(() => {
+        if ($('#oMobileWrapper').length && $('#oMobileWrapper').hasClass('slideout-panel')) {
+            clearInterval(createMenu);
+            return;
+        }
+        if ($('#oMobileWrapper').length && $('#toolbarcomponent') && $('#contentWrapper').length && $('#links').length && $('#rechts').length) {
+            $('#oMobileWrapper').append($('#contentWrapper'));
 
-        var leftSlideout = new Slideout({
-            'panel': document.getElementById('panel'),
-            'menu': document.getElementById('leftMenu'),
-            'padding': 256,
-            'tolerance': 70
-        });
+            $('#links').css('overflow', 'hidden');
+            $('#toolbarcomponent').css('display', '');
+            var leftSlideout = new Slideout({
+                'panel': document.getElementById('oMobileWrapper'),
+                'menu': document.getElementById('links'),
+                'padding': 130,
+                'tolerance': 70,
+                'side': 'left'
+            });
 
-        var rightSlideout = new Slideout({
-            'panel': document.getElementById('panel'),
-            'menu': document.getElementById('rightMenu'),
-            'padding': 256,
-            'tolerance': 70,
-            'side': 'right'
-        });
-        clearInterval(createMenu);
-    }
-}, 1000);
+            $('#rechts').css('top', '0px');
+            var rightSlideout = new Slideout({
+                'panel': document.getElementById('oMobileWrapper'),
+                'menu': document.getElementById('rechts'),
+                'padding': 140,
+                'tolerance': 70,
+                'side': 'right'
+            });
+
+            clearInterval(createMenu);
+        }
+    }, 10);
+
+    var adaptPlanetSize = setInterval(() => {
+        if ($('#countColonies').length) {
+            const nPlanets = parseInt($('#countColonies').text().split('/')[0]);
+            if (nPlanets) {
+                clearInterval(adaptPlanetSize);
+                console.log('found ' + nPlanets + ' planets ' + 100/nPlanets + 'vh');
+                $('#planetList .smallplanet').css('height', Math.floor(100/nPlanets) + 'vh');
+            }
+        }
+    }, 10);
+})(jQuery);
