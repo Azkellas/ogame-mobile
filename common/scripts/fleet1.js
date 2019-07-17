f44091941619bfa147b2bc45a51acabbbe24b0dc
Az <e.desbois@hotmail.fr>
2019-07-18 00:19:50 +0200

    Add highscore, minor fixes

--- common/scripts/fleet1.js
+++ common/scripts/fleet1.js
@@ -1,4 +1,10 @@
 'use strict';
 (($) => {
-
+    var setHCivilshipsTop = setInterval(() => {
+        if ($('.h_civilships').length) {
+            const height = $( window ).width() < 344 ? 350 : 230;
+            $('.h_civilships').css('top', height);
+            clearInterval(setHCivilshipsTop);
+        }
+    });
 })(jQuery);
