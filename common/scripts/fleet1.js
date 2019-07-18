f754fdac36ecd32abb6fc006379dac07b2e8b25c
Az <e.desbois@hotmail.fr>
2019-07-18 19:53:13 +0200

    Prevent auto scroll for fleet pages

--- common/scripts/fleet1.js
+++ common/scripts/fleet1.js
@@ -7,4 +7,16 @@
             clearInterval(setHCivilshipsTop);
         }
     });
+
+    var firstScrollPrevented = false;
+    $(window).scroll(event => {
+        if (firstScrollPrevented) {
+            // user scroll
+            return true;
+        } else {
+            // auto scroll to 'continue' button at load
+            window.scrollTo(0, 0);
+            firstScrollPrevented = true;
+        }
+    });
 })(jQuery);
--- common/scripts/fleet2.js
+++ common/scripts/fleet2.js
@@ -1,4 +1,15 @@
 'use strict';
 (($) => {
+    var firstScrollPrevented = false;
+    $(window).scroll(event => {
+        if (firstScrollPrevented) {
+            // user scroll
+            return true;
+        } else {
+            // auto scroll to 'continue' button at load
+            window.scrollTo(0, 0);
+            firstScrollPrevented = true;
+        }
+    });
 
 })(jQuery);
